536d29295c7257dd794c1777cf0ba0e1fd693321
Julien Huriez <syntaxlb@gmail.com>
2014-03-19 15:12:55 +0100

    Update migrations.php

--- fuel/app/config/development/migrations.php
+++ fuel/app/config/development/migrations.php
@@ -6,10 +6,7 @@ return array(
 		array(
 			'default' => 
 			array(
-				0 => '001_create_category',
-				1 => '002_create_post',
-				2 => '003_create_comment',
-				3 => '004_create_user',
+
 			),
 		),
 		'module' => 
@@ -19,16 +16,7 @@ return array(
 		array(
 			'auth' => 
 			array(
-				0 => '001_auth_create_usertables',
-				1 => '002_auth_create_grouptables',
-				2 => '003_auth_create_roletables',
-				3 => '004_auth_create_permissiontables',
-				4 => '005_auth_create_authdefaults',
-				5 => '006_auth_add_authactions',
-				6 => '007_auth_add_permissionsfilter',
-				7 => '008_auth_create_providers',
-				8 => '009_auth_create_oauth2tables',
-				9 => '010_auth_fix_jointables',
+
 			),
 		),
 	),
