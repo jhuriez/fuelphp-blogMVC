50e37b50a1c069855d79d70793f64202e6cb5cdc
jhuriez <syntaxlb@gmail.com>
2014-03-19 09:50:27 +0100

    [UPDATE] Routes fix

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -4,6 +4,10 @@ return array(
 	'_404_'   => 'frontend/index/404',    // The main 404 route
 
 	'backend' => 'backend/post',
+	'backend/(:any)' => 'backend/$1',
 
+	'user/login' => 'user/login',
+	'user/logout' => 'user/logout',
+	'favicon' => 'favicon',
 	'(:any)' => 'frontend/post/show/$1',
 );
