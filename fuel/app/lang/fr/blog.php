3fd802a18120dfddb4ad67fc02fd22f3d6adec1a
jhuriez <syntaxlb@gmail.com>
2014-03-19 14:04:31 +0100

    [NEW] French language

---
+++ fuel/app/lang/fr/blog.php
@@ -0,0 +1,85 @@
+<?php
+
+return array(
+	'page-not-found' => 'Page introuvable!',
+	'error' => 'Une erreur s\'est produite',
+	'category' => 'Categorie',
+	'categories' => 'Categories',
+	'commentaires' => 'Commentaires',
+	'on' => 'le',
+	'by' => 'par',
+	'submit' => 'Envoyer',
+	'last-posts' => 'Dernières publications',
+	'welcome' => 'Bienvenue sur mon blog',
+
+
+	'frontend' => array(
+		'post' => array(
+			'empty' => 'Aucune publication',
+			'not-found' => 'Publication introuvable',
+		),
+		'comment' => array(
+			'added' => 'Commentaire ajouté',
+		),
+		'category' => array(
+			'not-found' => 'Categorie introuvable',
+		),
+		'read-more' => 'Voir plus',
+		'comment-this-post' => 'Commenter cette publication',
+	),
+
+	'backend' => array(
+		'post' => array(
+			'manage' => 'Gestions des publications',
+			'empty' => 'Aucune publication',
+			'add' => 'Ajouter une nouvelle publication',
+			'edit' => 'Modifier une publication',
+			'added' => 'Publication créée',
+			'edited' => 'Publication mise à jour',
+			'deleted' => 'Publication supprimée',
+		),
+		'actions' => 'Actions',
+		'edit' => 'Modifier',
+		'add' => 'Ajouter',
+		'delete' => 'Supprimer',
+		'are-you-sure' => 'Êtes vous sûre ?',
+		'back-to-post' => 'Revenir aux publications',
+	),
+
+	'post' => array(
+		'model' => array(
+			'id' => 'ID',
+			'name' => 'Titre',
+			'category' => 'Categorie',
+			'slug' => 'Slug',
+			'content' => 'Contenu',
+			'publication-date' => 'Date de publication',
+			'category_id' => 'Categorie',
+			'user_id' => 'Auteur',
+		),
+	),
+
+	'comment' => array(
+		'model' => array(
+			'id' => 'ID',
+			'username' => 'Pseudo',
+			'mail' => 'E-mail',
+			'content' => 'Contenu',
+		),
+	),
+
+	'user' => array(
+		'login' => array(
+			'not-logged' => 'Vous n\'êtes pas connecté',
+			'failure' => 'Une erreur s\'est produite lors de la connexion',
+			'already-logged-in' => 'Vous êtes déjà connecté',
+			'logged-out' => 'Vous êtes déconnecté',
+			'sign-in' => 'Se connecter',
+		),
+		'model' => array(
+			'username' => 'Pseudo',
+			'password' => 'Mot de passe',
+		),
+	),
+
+);
